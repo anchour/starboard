9a650288d94a5183f102505266d29076a4e8a673
Antonio <antonio@anchour.com>
2023-03-13 09:24:19 -0400

    switch to insert instead of modify

--- packages/starboard-components/rollup.config.js
+++ packages/starboard-components/rollup.config.js
@@ -1,5 +1,6 @@
-import copy from 'rollup-plugin-copy'
-import modify from 'rollup-plugin-modify'
+import copy from 'rollup-plugin-copy';
+import insert from 'rollup-plugin-insert';
+
 
 export default {
 	// input: 'css/components/button/button-config.css',
@@ -10,15 +11,18 @@ export default {
   plugins: [
     copy({
       targets: [
-        { src: 'css/components/button/button-config.css', dest: '../../src/css/components' },
-        { src: 'liquid/components/button/button.liquid', dest: '../../shopify/snippets' }
+        { src: path.resolve(__dirname, './css/components/button/button-config.css'), dest: path.resolve(process.cwd(), './src/css/components') },
+        { src: path.resolve(__dirname, './liquid/components/button/button.liquid'), dest: path.resolve(process.cwd(), './shopify/snippets') }
         // { src: ['assets/fonts/arial.woff', 'assets/fonts/arial.woff2'], dest: 'dist/public/fonts' },
         // { src: 'assets/images/**/*', dest: 'dist/public/images' }
       ]
     }),
-    modify({
-      find: '@import "tailwindcss/components";',
-      replace: '@import "tailwindcss/components";\n/** Anchour Components */\nimport "@anchour/starboard/css/components/button/index";\nimport "components/button";'
-    })
+    insert.append(
+      'import "@anchour/starboard/css/components/button/index";\nimport "components/button";',
+      {
+        include: "path.resolve(process.cwd(), './src/css/main.css')",
+      },
+    )
+    
   ]
 };
