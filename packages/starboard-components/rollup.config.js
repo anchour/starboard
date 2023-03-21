ee6e77e9e8a2ce3604aaecef2c2bc2b0ffbdd885
Antonio <antonio@anchour.com>
2023-03-21 12:00:38 -0400

    rollup stuff

--- packages/starboard-components/rollup.config.js
+++ packages/starboard-components/rollup.config.js
@@ -1,6 +1,11 @@
 import copy from 'rollup-plugin-copy';
 import insert from 'rollup-plugin-insert';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 export default {
@@ -11,7 +16,7 @@ export default {
   plugins: [
     copy({
       targets: [
-        { src: path.resolve(__dirname, './css/components/button/button-config.css'), dest: path.resolve(process.cwd(), './src/css/components') },
+        { src: path.resolve(__dirname, './css/components/button/button.css'), dest: path.resolve(process.cwd(), './src/css/components') },
         { src: path.resolve(__dirname, './liquid/components/button/button.liquid'), dest: path.resolve(process.cwd(), './shopify/snippets') }
         // { src: ['assets/fonts/arial.woff', 'assets/fonts/arial.woff2'], dest: 'dist/public/fonts' },
         // { src: 'assets/images/**/*', dest: 'dist/public/images' }
